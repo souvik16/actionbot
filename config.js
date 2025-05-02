e7866386ac765d1b464ca767fa918eed54ccf63e
souvik16 <samanta.souvik@gmail.com>
2025-05-02 21:29:54 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  SUMMARIZATION_API_URL: "https://souvik-16-souvik-langflow-experiment.hf.space/api/v1/run/1f68282e-e334-43d2-9101-ff286472449b",
+  CHATBOT_API_URL: "https://souvik-16-souvik-langflow-experiment.hf.space/api/v1/run/8b477873-42db-4d87-bcf1-3be445162ea1",
+  API_TOKEN: "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"
+};
